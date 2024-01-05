c469b38322a6e16c95507b7b548f28a56fbbf4a3
Henrique Santos <henrique.santos@westwing.com.br>
2024-01-04 22:37:57 -0300

    Add router sub package

--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
+import "Api_first_1_19/router"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on
+	router.Initialize()
 }
