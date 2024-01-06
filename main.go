26757dfbf2298a1dbb575fc273703d3b6c26fd59
Henrique Santos <henrique.santos@westwing.com.br>
2024-01-05 22:54:32 -0300

    add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "Api_first_1_19/router"
+import (
+	"Api_first_1_19/config"
+	"Api_first_1_19/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config init error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
