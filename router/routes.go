e199f96661ad46aa2c5dacaefd0d570f1cc38b79
Henrique Santos <henrique.santos@westwing.com.br>
2024-01-06 15:07:58 -0300

    fix routes GET POST

--- router/routes.go
+++ router/routes.go
@@ -10,8 +10,8 @@ func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", handler.CreateOpeningHandler)
-		v1.POST("/opening", handler.ShowOpeningHandler)
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningHandler)
